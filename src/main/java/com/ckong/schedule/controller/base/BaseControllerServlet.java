e06b4cf2f2d39a69569472d7113bf771979b8a73
Danger <oncwnuK5dPCGTs5EZ6eIqFTqMw38@git.weixin.qq.com>
2019-09-07 20:30:01 +0800

    添加更新课程功能

--- src/main/java/com/ckong/schedule/controller/base/BaseControllerServlet.java
+++ src/main/java/com/ckong/schedule/controller/base/BaseControllerServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.sql.SQLException;
+import java.util.Optional;
 
 
 /**
@@ -34,24 +36,26 @@ public abstract class BaseControllerServlet extends HttpServlet {
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("application/json;charset=UTF-8");
 
-        this.getServletContext().getContextPath();
+        // 调用的方法不能为空
+        String methodName = Optional
+                .ofNullable(req.getParameter("method"))
+                .map(String::trim)
+                .orElse("toNotFoundMethodErrorPage");
 
-        String parameter = req.getParameter("method").trim();
         Method invokeMethod;
         try {
-            invokeMethod = this.getClass().getMethod(parameter, HttpServletRequest.class, HttpServletResponse.class);
+            invokeMethod = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
         } catch (NoSuchMethodException e) {
             ResponseJson res = new ResponseJson();
-            res.setMessage("您所调用的方法名不存在: " + parameter);
+            res.setMessage("您所调用的方法名不存在: " + methodName);
             Writer writer = new OutputStreamWriter(resp.getOutputStream());
             try (PrintWriter out = new PrintWriter(writer)) {
                 out.print(JSON.toJSONString(res));
             }
-            throw new ServletException(parameter + "方法不存在");
+            throw new ServletException(methodName + "方法不存在");
         }
 
         try {
-            invokeMethod.setAccessible(true);
 
             ResponseJson result = (ResponseJson) invokeMethod.invoke(this, req, resp);
             System.out.println("Start....");
@@ -61,9 +65,20 @@ public abstract class BaseControllerServlet extends HttpServlet {
             try (PrintWriter out = new PrintWriter(writer)) {
                 out.print(str);
             }
-        } catch (IllegalAccessException | InvocationTargetException e) {
+
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
+        } catch (InvocationTargetException e ) {
+            // 处理项目中抛出的SQLException
+            if (e.getTargetException() instanceof SQLException ) {
+                e.printStackTrace();
+            }
         }
 
     }
+
+    public void toNotFoundMethodErrorPage(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.sendRedirect( req.getContextPath() + "/static/NotFoundMethodPage.jsp");
+    }
+
 }
